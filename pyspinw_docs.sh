cc37c87b33398ebef2b45c1781df9bbddea36747
lucas-wilkins <lucas.wilkins@stfc.ac.uk>
2026-04-12 04:25:27 +0100

    Cleaned scripts

--- pyspinw_docs.sh
+++ pyspinw_docs.sh
@@ -1,6 +1,14 @@
+if [[ $# -eq 1 && "$1" == "rebuild" ]]; then
+  echo "Rebuild flag is set"
+  REBUILD=true
+else
+  REBUILD=false
+fi
+
 # Warn about dangers
 
 echo "Warning: If it is possible that this script could delete things you don't want it to. Make sure things are backed up."
+echo "Note: use '$0 rebuild' to clean directories first... more unsafe"
 echo
 read -p "Are you sure? (Y/[N])" -n 1 -r
 echo    
@@ -8,29 +16,39 @@ echo
 # Check confirmation
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    # Clean directory    
     mkdir -p docsbuild
     cd docsbuild
-    #rm -rf *
+    
+    if [[ "$REBUILD" == "true" ]]; then
+    
+        # Clean directory    
+        rm -rf *
 
-    # Get git repo
-    #git clone https://github.com/SpinW/pySpinW/
+        # Get git repo
+        git clone https://github.com/SpinW/pySpinW/
+    fi
 
     cd pySpinW
-    export PYTHONPATH="./pyspinw/:$PYTHONPATH"
-
-    # Set up venv
-    echo "Setting up VENV"
-    #python3 -m venv .venv
-    source .venv/bin/activate
-
-    # Get required packages
-    echo "Getting pdoc"
-    #python -m pip install pdoc
-
-    echo "Getting dependencies"
-    #python -m pip install -r requirements.txt
     
+
+    if [[ "$REBUILD" == "true" ]]; then
+        export PYTHONPATH="./pyspinw/:$PYTHONPATH"
+
+        # Set up venv
+        echo "Setting up VENV"
+        python3 -m venv .venv
+        source .venv/bin/activate
+
+        # Get required packages
+        echo "Getting pdoc"
+        python -m pip install pdoc
+
+        echo "Getting dependencies"
+        python -m pip install -r requirements.txt
+
+    fi
+
+
     # Get ready to build
     echo "Building"
     python -m pdoc pyspinw --no-show-source -o ../docs
